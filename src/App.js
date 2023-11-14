93a90fc16c643800fb47f972f051d531a164971e
Drina2310 <drinarincon4@gmail.com>
2023-11-14 16:47:07 -0300

    reset the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-8">
+          <h2>Lista de Tareas</h2>
+        </div>
+        <div className="col-4">
+          <h2>Formulario</h2>
+        </div>
+      </div>
     </div>
   );
 }
